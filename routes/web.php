0d6ecb16b6d9b949f5292a42308f02a26e743be9
unknown <you@example.com>
2022-04-05 11:42:47 +0700

    uwuw

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+Route::get('/uwuw', function () {
+	return view('welcome');
+});
+
